dbb047ee90e8724de24878e24c8b1dd5ed2dc3e2
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-11 00:18:53 -0300

    Create Main.java

---
+++ Questao3/Main.java
@@ -0,0 +1,27 @@
+ public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        Lutador ryu = new Lutador("Ryu",100,10);
+        Lutador bison = new Lutador("bison",100,12);
+        ryu.aplicarGolpe(bison);
+        ryu.aplicarGolpe(bison);
+        ryu.aplicarGolpe(bison);
+
+        System.out.println();
+        System.out.println("Bison está com: "+bison.energia+" de energia.");
+
+        System.out.println();    
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        bison.aplicarGolpe(ryu);
+        
+        System.out.println();
+        System.out.println("Ryu está com: "+ryu.energia+" de energia.");
+
+    }
+}
