29ed6553066b1c6f6d25698dcb8b9199aa993f1a
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-11 00:13:27 -0300

    Create Endereco.java

---
+++ Questao2/Endereco.java
@@ -0,0 +1,21 @@
+public class Endereco {;
+
+    public String rua;
+    public String numero;
+    public String complemento;
+    public String bairro;
+    public String cidade;
+    public String uf;
+    public String cep;
+
+     public Endereco(String rua, String numero, String complemento, String bairro, String cidade, String uf, String cep) {
+    this.rua = rua;
+    this.numero = numero;
+    this.complemento = complemento;
+    this.bairro = bairro;
+    this.cidade = cidade;
+    this.uf = uf;
+    this.cep = cep;
+    
+    }
+}
