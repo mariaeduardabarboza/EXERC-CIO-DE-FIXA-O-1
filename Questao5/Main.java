faff586349776fd0866ee3d6c6d2bb9cf418e507
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-13 19:24:29 -0300

    Update Main.java

--- Questao5/Main.java
+++ Questao5/Main.java
@@ -1,7 +1,5 @@
 public class Main {
       public static void main(String[] args) {
-       
-   System.out.println("                              ");
 
   Aluno aluno = new Aluno("Maria Eduarda Barboza Santos", "03896585598");
 
