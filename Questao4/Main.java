58444c40e9655c14fa15a4612bb702b58e5161f3
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-13 16:21:24 -0300

    Update Main.java

--- Questao4/Main.java
+++ Questao4/Main.java
@@ -1,6 +1,6 @@
 public class Main{ 
 	  public static void main(String[] args) {
-	  Post post = new Post("FORA BOLSONARO","https://edition.cnn.com/");
+	  Post post = new Post("Find the latest breaking news and information on the top stories, weather, business, entertainment, politics, and more.","https://edition.cnn.com/");
     
     post.curtir();
     post.curtir();
@@ -14,4 +14,5 @@ public class Main{
   System.out.println("Número de Compartilhamentos:"+post.numeroCompartilhamentos);
     }
   }
+  
   
