4d43c8a864c8ba85b0c57c5b14e0a8e8fd58e312
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-13 19:23:22 -0300

    Update Aluno.java

--- Questao5/Aluno.java
+++ Questao5/Aluno.java
@@ -1,11 +1,12 @@
-public class Aluno {
+ public Aluno
+ public class Aluno {
 	
 	    String nome;
 	    String cpf ;
 	    double nota1;
 	    double nota2;
 	    double nota3;
- public Aluno(String nome, String cpf) {
+      public Aluno(String nome, String cpf) {
 
     this.nome = nome;
     this.cpf = cpf;
@@ -13,17 +14,4 @@ public class Aluno {
     this.nota2 = 0;
     this.nota2 = 0;
 }
-
- public void setNota1(double nota1) {
- this.nota1 = nota1;
  }
-  public void setNota2(double nota2) {
- this.nota2 = nota2;
- }
-  public void setNota3(double nota3) {
- this.nota3 = nota3;
- }
- public double calcularMedia() {
-return (nota1+nota2+nota3) / 3;
- }
-}
